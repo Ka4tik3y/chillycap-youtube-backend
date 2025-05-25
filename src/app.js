b929855ac05408871a33b5ac1434e34db373fbe7
Kartikey <kartikey442004@gmail.com>
2025-05-25 16:51:13 +0530

    healthcheck okay

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ app.use(cookieParser());
 
 
 import userRouter from "./routes/user.routes.js"
-// import healthcheckRouter from "./routes/healthcheck.routes.js"
+import healthcheckRouter from "./routes/healthcheck.routes.js"
 // import tweetRouter from "./routes/tweet.routes.js"
 import subscriptionRouter from "./routes/subscription.routes.js"
 import videoRouter from "./routes/video.routes.js"
@@ -29,7 +29,7 @@ import likeRouter from "./routes/like.routes.js"
 
 app.use("/api/v1/users", userRouter)
 
-// app.use("/api/v1/healthcheck", healthcheckRouter)
+app.use("/api/v1/healthcheck", healthcheckRouter)
 // app.use("/api/v1/tweets", tweetRouter)
 app.use("/api/v1/subscriptions", subscriptionRouter)
 app.use("/api/v1/videos", videoRouter)
